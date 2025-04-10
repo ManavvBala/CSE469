ef30683819878f4ffbefda8c0c0d2a9e0eb0e9a7
ManavvBala <manavvbalakrishnan@live.com>
2025-04-09 17:36:50 -0700

    DFF enable dnoe

--- Lab1/DFF_enable.sv
+++ Lab1/DFF_enable.sv
@@ -2,5 +2,12 @@ module DFF_enable (
     output logic q;
     input logic d, reset, clk, enable;
 );
+    logic temp_q, temp_d;
+    // instantiate dff to hold state
+    D_FF dff (.d(temp_d), .q(temp_q), .*);
+
+    mux2_1 mux (.sel(enable), .d({temp_q, d}), .q(temp_d));
+
+    assign q = temp_q;
 
 endmodule
