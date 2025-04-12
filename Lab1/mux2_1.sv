edc4ee3093812ba7e950c45813155150680668d2
hitesh M <mantha.hitesh@gmail.com>
2025-04-11 21:04:34 -0700

    huh

--- Lab1/mux2_1.sv
+++ Lab1/mux2_1.sv
@@ -1,3 +1,4 @@
+`timescale 1ns/10ps
 module mux2_1 (
     input logic sel,
     input logic [1:0] d,
@@ -8,15 +9,15 @@ module mux2_1 (
     // need to hold both outputs of the ands
     logic [1:0] and_results;
 
-    and (#50ps) AND_1 (and_results[1], sel, d[1]);
+    and #(50ps) AND_1 (and_results[1], sel, d[1]);
     
     // need to hold ~sel
 
-    logic not_sel
-    not (#50ps) NOT1 (not_sel, sel)
-    and (#50ps) AND_0 (and_results[0], not_sel, d[0]);
+    logic not_sel;
+    not #(50ps) NOT1 (not_sel, sel);
+    and #(50ps) AND_0 (and_results[0], not_sel, d[0]);
 
     // out
-    or (#50ps) OR (q, and_results[0], and_results[1]);
+    or #(50ps) OR (q, and_results[0], and_results[1]);
 
 endmodule 
