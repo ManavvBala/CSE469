be9dedd68b038ba8e47a1780db2833d9bbb2156f
hitesh M <mantha.hitesh@gmail.com>
2025-04-28 12:29:14 -0700

    fixed reg file

--- Lab1/DFF_enable.sv
+++ Lab1/DFF_enable.sv
@@ -2,12 +2,15 @@ module DFF_enable (
     output logic q,
     input logic d, reset, clk, enable
 );
-    logic temp_q, temp_d;
+    logic out;
+    logic [1:0] in;
+
+    assign in[0] = q; // Feed back the actual output
+    assign in[1] = d;
+    
     // instantiate dff to hold state
-    D_FF dff (.d(temp_d), .q(temp_q), .*);
+    D_FF dff (.d(out), .q(q), .*);
 
-    mux2_1 mux (.sel(enable), .d({temp_q, d}), .q(temp_d));
-
-    assign q = temp_q;
+    mux2_1 mux (.sel(enable), .d(in[1:0]), .q(out));
 
 endmodule 
